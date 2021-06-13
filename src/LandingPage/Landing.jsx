630aa1166dd23b103ebb2a320891c61f9eee7608
Ivan Felix <c.felixivan@outlook.com>
2021-06-13 17:37:00 -0600

    landing 2 sections

--- src/LandingPage/Landing.jsx
+++ src/LandingPage/Landing.jsx
@@ -5,21 +5,40 @@ import IconBrujula from '../Assets/logo-w-name.png';
 import Astronaut from '../Assets/astro.png';
 import LogoBrujula from '../Assets/logos-brujula-icon.png';
 const Landing = () => {
+  const infoline1 = [
+    {
+      tittle: 'Análisis de Mercado',
+      info: 'Realizamos investigación de tu mercado, tu publico objetivo y tu competencia para poder segmentar y llegar más efectivamente a ellos.',
+    },
+    {
+      tittle: 'Diseño de Identidad',
+      info: 'Tu negocio necesita una identidad propia, la cual será aplicada a todas sus redes, imágenes y aplicaciones, no solo es la foto si no la manera de comunicar un mensaje.',
+    },
+    {
+      tittle: 'Análisis de Mercado',
+      info: 'Manejamos Facebook, Instram, Google, Twitter, Linkeding, Youtube, Tiktok, Email y todo tipo de aplicaciones especializadas.',
+    },
+  ];
   return (
     <Col className='container m-0'>
       <Row className='navbar'>
         <img
           alt=''
           src={IconBrujula}
-          style={{ width: 250, height: 72, paddingTop: 10, paddingBottom: 10 }}
+          style={{ width: 165, height: 50, paddingTop: 5, paddingBottom: 5 }}
         />
       </Row>
       <Row className='header'>
-        <span className='subtitle-header'>
-          Deja tu marketing digital en buenas manos
-        </span>
-        <span className='title-header'>¡Hagamos crecer</span>
-        <span className={'title-down-header'}>tu negocio!</span>
+        <Col className='header-container'>
+          <span className='subtitle-header'>
+            Deja tu marketing digital en buenas manos
+          </span>
+          <span className='title-header'>¡Hagamos crecer</span>
+          <span className={'title-down-header'}>tu negocio!</span>
+          <Col style={{ position: 'relative' }}>
+            <img alt='' className='astronaut' src={Astronaut}></img>
+          </Col>
+        </Col>
 
         <Row className='logo-brujula'>
           <img
@@ -32,8 +51,25 @@ const Landing = () => {
           />
           <p>El mejor momento para iniciar es hoy</p>
         </Row>
-
-        <img alt='' src={Astronaut} className='astronaut'></img>
+      </Row>
+      <Row className='info-section'>
+        <h2 className='info-title'>El Marketing Digital consiste en</h2>
+        <Row className={'info-container'}>
+          {infoline1.map((info) => (
+            <Col className='info-item'>
+              <h2>{info.tittle}</h2>
+              <p>{info.info}</p>
+            </Col>
+          ))}
+        </Row>
+        <Row className={'info-container'}>
+          {infoline1.map((info) => (
+            <Col className='info-item'>
+              <h2>{info.tittle}</h2>
+              <p>{info.info}</p>
+            </Col>
+          ))}
+        </Row>
       </Row>
     </Col>
   );
